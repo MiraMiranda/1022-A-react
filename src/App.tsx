ae755f56b661cb005bccbb3cf033fe32e9bb4d5f
MiraMiranda <rafael.silva25@estudante.ifms.edu.br>
2024-03-12 10:09:31 -0400

    Meu primeiro commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+
+function App() {
+  let nome = "Rafael"   
+  
+  return (
+    <h1 className="meu-nome">Ola eu sou {nome}</h1>
+  )
+}
+
+
+export default App
---
+++ src/componentes/MeuComponente.tsx
@@ -0,0 +1,4 @@
+//Faça um componente que faça um retângulo da largura da tela por 200xp crie uma variavel com let no js que contenha uma valor "Meu Componente"
+//crie uma div no componente que dentro dela mostre um paragrafo contendo o valor da variavel.
+
+function retangulo()
